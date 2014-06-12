fd680b6969be64dec6dd4a3767a98c65e9696048
Jaimin <jaiminpandya@example.com>
2014-06-12 14:43:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby on Rails Tutorial: demo
+
+This is the demo application for
+[*Agile Web Development with Rails*] by [Sam Ruby], [Dave Thomas], [David Heinemeier Hansson].
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '6f0299c1b08f071d98ce585df01fce607d5b5e440b2e5c380535255400c3492efb9c16e37ce4280eb20659dc5b26e6e29886421b43dea1f56d016012aa36bff3'
